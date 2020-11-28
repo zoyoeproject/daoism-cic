0010a0597aace8d7e1c0eda782c2d5eebdac2b45
xgao@zoyoe.com <xgao@zoyoe.com>
2020-11-28 20:26:24 +0800

    add bool type

--- src/coreType.ml
+++ src/coreType.ml
@@ -2,7 +2,10 @@ open Names
 let core_dir = ModPath.MPfile (DirPath.make [Id.of_string "core"])
 let c_int_type = Constant.make core_dir (Label.of_string "int")
 let c_float_type = Constant.make core_dir (Label.of_string "float")
+let i_bool = KerName.make core_dir (Label.of_string "bool")
+
 let int_type = Constr.Const (c_int_type, 0)
 let float_type = Constr.Const (c_float_type, 0)
+let bool_type = Constr.mkInd (i_bool, 1)
 
 
