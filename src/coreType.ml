5f467f8864f4145eb237556b9147774c5c3adcb6
xgao@zoyoe.com <xgao@zoyoe.com>
2021-01-02 22:02:21 +0800

    add prod and pair utils

--- src/coreType.ml
+++ src/coreType.ml
@@ -1,9 +1,47 @@
 open Names
 let core_dir = ModPath.MPfile (DirPath.make [Id.of_string "core"])
+let c_type = Constant.make core_dir (Label.of_string "Type")
 let c_int_type = Constant.make core_dir (Label.of_string "int")
 let c_float_type = Constant.make core_dir (Label.of_string "float")
-let i_bool = KerName.make core_dir (Label.of_string "bool")
+let c_fst = Constant.make core_dir (Label.of_string "fst")
+let c_snd = Constant.make core_dir (Label.of_string "snd")
 
+let i_bool = KerName.make core_dir (Label.of_string "bool")
+let i_prod = KerName.make core_dir (Label.of_string "prod")
+
+let base_type = Constr.Const (c_type, 0)
 let int_type = Constr.Const (c_int_type, 0)
 let float_type = Constr.Const (c_float_type, 0)
 let bool_type = Constr.mkInd (i_bool, 0)
+let prod_type = Constr.mkInd (i_prod, 0)
+let fst = Constr.Const (c_fst, 0)
+let snd = Constr.Const (c_snd, 0)
+
+let pair = Constr.mkConstruct ((i_prod, 0), 0)
+
+let bool_ind = MiniCic.Mind.mk_enum_body (Id.of_string "bool")
+  [|(Id.of_string "true"); (Id.of_string "fase")|]
+
+let prod_ind =
+  let open Constr in
+  let open Mind in
+  let mk_name str = Name.mk_name (Id.of_string str) in
+  let cell = mk_record_cell (Id.of_string "prod")
+    [|"pair", (
+      mkProd (mk_name "T1", base_type,
+        (mkProd (mk_name "T2", base_type,
+          (mkProd (mk_name "x", mkRel 2,
+            (mkProd (mk_name "y", mkRel 2,
+              (mkApp (prod_type, [|mkRel 4; mkRel 3|]))
+            ))
+          ))
+        ))
+      ))
+   |]
+  in mk_inductive_body [|cell|] [] []
+
+let bootstrap_env =
+  let env = Env.empty_env in
+  env
+  |> Env.add_mutind i_bool bool_ind
+  |> Env.add_mutind i_prod prod_ind
---
+++ src/prod.ml
@@ -0,0 +1,33 @@
+(* Basic operators for Tuples in mini-cic *)
+
+exception OutOfTupleBoundary
+
+let mk_pair (x, tx, y, ty) =
+  Constr.mkApp (CoreType.pair, [|tx;ty;x;y|])
+
+let mk_prod_type types =
+  List.fold_right (fun c acc -> Constr.mkApp (CoreType.prod_type, [|c; acc|]))
+    (List.tl types) (List.hd types)
+
+let rec mk_pair eles =
+  List.fold_right (fun (c,t) (c', t') ->
+    Constr.mkApp (CoreType.pair, [|t; t'; c; c'|])
+    , Constr.mkApp (CoreType.prod_type, [|t; t'|])
+  ) (List.tl eles) (List.hd eles)
+
+(* Select the kth elements of e (t_1 * t2 * t3 * tk ....) *)
+let rec mk_select eles k e : Constr.t =
+  match eles with
+  | [] -> assert false
+  | [e, _] when k == 0 -> e
+  | [(_, hd); (_, tl)] when k == 0 ->
+    Constr.mkApp (CoreType.fst, [|hd;tl;e|])
+  | [(_, hd); (_, tl)] when k == 1 ->
+    Constr.mkApp (CoreType.snd, [|hd;tl;e|])
+  | (_, hd) :: tl when k < List.length eles ->
+      let e = mk_select tl (k-1) e in
+      let types = List.map (fun c -> snd c) tl in
+      Constr.mkApp (CoreType.snd, [|hd;mk_prod_type types;e|])
+  | _ -> assert false
+
+
