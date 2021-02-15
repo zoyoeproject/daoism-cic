b43749e48e924848e08ccbd6ef4edefb7202db0a
Heng Zhang <shindar90@gmail.com>
2021-02-15 11:13:18 +0000

    Add parse_select for tuple

--- src/prod.ml
+++ src/prod.ml
@@ -1,30 +1,54 @@
 (* Basic operators for Tuples in mini-cic *)
 
+open Constr
+open MiniCic.CoreType
+
 exception OutOfTupleBoundary
 
+let is_const_fst c = Constr.compare c fst_const = 0
+
+let is_const_snd c = Constr.compare c snd_const = 0
+
 let mk_prod_type types =
-  List.fold_right (fun c acc -> Constr.mkApp (CoreType.prod_type, [|c; acc|]))
+  List.fold_right
+    (fun c acc -> mkApp (prod_type, [|c; acc|]))
     (List.tl types) (List.hd types)
 
 let mk_pair eles =
-  List.fold_right (fun (c,t) (c', t') ->
-    Constr.mkApp (CoreType.pair, [|t; t'; c; c'|])
-    , Constr.mkApp (CoreType.prod_type, [|t; t'|])
-  ) (List.tl eles) (List.hd eles)
+  List.fold_right
+    (fun (c, t) (c', t') ->
+      ( mkApp (CoreType.pair, [|t; t'; c; c'|])
+      , mkApp (CoreType.prod_type, [|t; t'|]) ) )
+    (List.tl eles) (List.hd eles)
 
 (* Select the kth elements of e (t_1 * t2 * t3 * tk ....) *)
 let rec mk_select eles k e : Constr.t =
-  Js.log (Printf.sprintf "k is %d" k);
+  Js.log (Printf.sprintf "k is %d" k) ;
   match eles with
   | [] -> assert false
   | [_] when k == 0 -> e
   | (_, hd) :: tl when k == 0 ->
-    let types = List.map (fun c -> snd c) tl in
-    Constr.mkApp (CoreType.fst_const, [|hd;mk_prod_type types;e|])
-  | [(_, hd); (_, tl)] when k == 1 ->
-    Constr.mkApp (CoreType.snd_const, [|hd;tl;e|])
-  | (_, hd) :: tl when k < List.length eles ->
-      let e = mk_select tl (k-1) e in
       let types = List.map (fun c -> snd c) tl in
-      Constr.mkApp (CoreType.snd_const, [|hd;mk_prod_type types;e|])
+      mkApp (CoreType.fst_const, [|hd; mk_prod_type types; e|])
+  | [(_, hd); (_, tl)] when k == 1 -> mkApp (CoreType.snd_const, [|hd; tl; e|])
+  | (_, hd) :: tl when k < List.length eles ->
+      let e = mk_select tl (k - 1) e in
+      let types = List.map (fun c -> snd c) tl in
+      mkApp (CoreType.snd_const, [|hd; mk_prod_type types; e|])
   | _ -> assert false
+
+let is_select c =
+  match c with
+  | App (op, [|_; _; _|]) when is_const_snd op || is_const_snd op -> true
+  | _ -> false
+
+(* Parse the select of a tuple, return idx, type, and tuple constr *)
+let parse_select c =
+  let rec aux c idx =
+    match c with
+    | App (op, [|t; _; inner|]) when is_const_fst op -> (idx, t, inner)
+    | App (op, [|_; t; inner|]) when is_const_snd op ->
+        if is_select c then aux inner (idx + 1) else (idx + 1, t, inner)
+    | _ -> assert false
+  in
+  aux c 0
